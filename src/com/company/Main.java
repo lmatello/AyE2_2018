9290c0e9a17c0b981e9f8b5d864ef653e75898db
Lucas Matello <lucas.matello@fravega.com.ar>
2018-05-21 10:04:29 -0300

    refactor code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,13 +10,13 @@ public class Main {
 
         //Ejercicio 1
         Lamparita lamparita = new Lamparita();
-        System.out.println(String.valueOf(lamparita.getEstado()));
+        System.out.println("La lamparita esta: " + lamparita.getEstado());
 
         lamparita.encender();
-        System.out.println(String.valueOf(lamparita.getEstado()));
+        System.out.println("La lamparita esta: " + lamparita.getEstado());
 
         lamparita.apagar();
-        System.out.println(String.valueOf(lamparita.getEstado()));
+        System.out.println("La lamparita esta: " + lamparita.getEstado());
 
     }
 }
